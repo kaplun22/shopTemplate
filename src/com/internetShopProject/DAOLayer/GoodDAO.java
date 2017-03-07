a193b819d8070c3a7239ff7b1ea0dc596a1223cd
kaplun22 <kaplun22@hotmail.com>
2017-03-07 20:46:56 +0200

    1

--- src/com/internetShopProject/DAOLayer/GoodDAO.java
+++ src/com/internetShopProject/DAOLayer/GoodDAO.java
@@ -1,6 +1,8 @@
 package com.internetShopProject.DAOLayer;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -10,12 +12,25 @@ import java.util.logging.Logger;
 public class GoodDAO {
     private static Logger logger = Logger.getLogger(GoodDAO.class.getName());
     public static void create(String name, int price){
+       Connection con = null;
+        PreparedStatement pr = null;
+
         try {
-            Connection connect =  DBConnect.getConnectionToDB();
+           con =  DBConnect.getConnectionToDB();
         } catch (Exception e) {
             logger.log(Level.SEVERE,"Connection isn`t succesfull");
-        
+
         }
+        String sql = "INSERT INTO Goods(name, price) VALUES(?,?)";
+        try {
+            pr.setString(1,name);
+            pr.setInt(2,price);
+            pr.executeQuery();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+
 
     }
 
